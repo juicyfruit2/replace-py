cf2b5a8fbdc51b2c6086645d50f7b5513f56d095
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 15:37:57 +0200

    Add files via upload

---
+++ repalace.py
@@ -0,0 +1,5 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!"
+print(sentence.replace( "!" , " " ))
+print("the quick brown fox jumps over the lazy dog".upper())
+spam = "the quick brown fox jumps over the lazy dog".upper()
+print(spam[::-1])
